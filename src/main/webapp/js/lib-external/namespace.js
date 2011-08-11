80911f57b055e12da7f7809482bf8026fe45cf8d
Lars Brandt <larsbrandt@gmail.com>
2011-08-11 17:33:23 +0200

    base development version

---
+++ src/main/webapp/js/lib-external/namespace.js
@@ -0,0 +1,23 @@
+//Defines top level namespace
+var bekk = (function () {
+
+  function namespace(string) {
+    var object = this;
+    var levels = string.split(".");
+
+    for (var i = 0, l = levels.length; i < l; i++) {
+      if (typeof object[levels[i]] == "undefined") {
+        object[levels[i]] = {};
+      }
+
+      object = object[levels[i]];
+    }
+
+    return object;
+  }
+
+  return {
+    namespace: namespace
+  };
+}());
+
