bb3c9756ffda5be3e410c1793fd5d749aca3a292
Christian Schwarz <christian.schwarz@bekk.no>
2011-08-16 10:01:21 +0200

    reverted back to completed state

--- src/main/webapp/js/pricePage/controller.js
+++ src/main/webapp/js/pricePage/controller.js
@@ -1,10 +1,27 @@
-describe('', function() {
+(function() {
+//Controller
 
-  beforeEach(function() {
-  });
-    
-  it('should ', function() {
-    
-  });
 
-});
+  function init(formView, priceView, pageObject) {
+    this.formView = formView;
+    this.priceView = priceView;
+
+    formView.init(this, pageObject);
+    priceView.init(this, pageObject);
+
+  }
+
+  function fetchPrice(weight) {
+    var states = booking.pricePage.states;
+    var price = weight * 3;
+    this.notify(states.priceReady, { price : price });
+  }
+
+
+  booking.namespace("pricePage").controller = booking.extend(
+      {
+        init : init,
+        fetchPrice : fetchPrice
+      },
+      booking.util.observable);
+}());
---
+++ src/main/webapp/js/pricePage/priceView.js
@@ -0,0 +1,20 @@
+(function() {
+//PriceView
+
+  function showPrice(result) {
+    this.priceLabel.html(result.price);
+  }
+  
+  function init(controller, pageObject) {
+    this.priceLabel = pageObject.priceLabel;
+
+    var states = booking.pricePage.states;
+    controller.observe(states.priceReady, this.showPrice.bind(this));
+  }
+
+  booking.namespace("pricePage").priceView = {
+    init : init,
+    showPrice : showPrice
+  };
+  
+}());
